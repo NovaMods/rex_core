04740667b9f93af672fcb901872324d488892977
David Dubois <dexcelstraun7@gmail.com>
2020-03-16 09:40:09 -0700

    [logger] Help MSVC with name resolution

--- include/rx/core/assert.cpp
+++ include/rx/core/assert.cpp
@@ -1,53 +1,55 @@
-#include <stdlib.h> // abort
 #include <stdarg.h> // va_{list,start,end,copy}
-#include <stdio.h> // vsnprintf
+#include <stdio.h>  // vsnprintf
+#include <stdlib.h> // abort
 
 #if defined(RX_BREAK_ON_ASSERT)
 #if defined(RX_PLATFORM_WINDOWS)
-#include <Windows.h>   // DebugBreak
+#include <Windows.h> // DebugBreak
 #elif defined(RX_PLATFORM_LINUX)
 #include <signal.h> // raise, SIGINT
 #endif
 #endif
 
-#include "rx/core/log.h" // RX_LOG, rx::log
 #include "rx/core/abort.h" // rx::abort
+#include "rx/core/log.h"   // RX_LOG, rx::log
 
 RX_LOG("assert", logger);
 
 namespace rx {
 
-[[noreturn]]
-void assert_fail(const char* _expression,
-  const source_location& _source_location, const char* _message, ...)
-{
-  va_list va;
-  va_start(va, _message);
+    [[noreturn]] void assert_fail(const char* _expression, const source_location& _source_location, const char* _message, ...) {
+        va_list va;
+        va_start(va, _message);
 
-  // Determine how much memory is needed to format the assertion message.
-  va_list ap;
-  va_copy(ap, va);
-  const int length = vsnprintf(nullptr, 0, _message, ap);
-  va_end(ap);
+        // Determine how much memory is needed to format the assertion message.
+        va_list ap;
+        va_copy(ap, va);
+        const int length = vsnprintf(nullptr, 0, _message, ap);
+        va_end(ap);
 
-  // Construct assertion message.
-  string contents;
-  contents.resize(length);
-  vsnprintf(contents.data(), contents.size() + 1, _message, va);
-  va_end(va);
+        // Construct assertion message.
+        string contents;
+        contents.resize(length);
+        vsnprintf(contents.data(), contents.size() + 1, _message, va);
+        va_end(va);
 
-  logger(log::level::k_error, "Assertion failed: %s (%s:%d %s) \"%s\"",
-    _expression, _file, _line, _function, utility::move(contents));
+        // Write the assertion failure to the logger.
+        ::logger->error("Assertion failed: %s (%s:%d %s) \"%s\"",
+                      _expression,
+                      _source_location.file(),
+                      _source_location.line(),
+                      _source_location.function(),
+                      contents);
 
 #if defined(RX_BREAK_ON_ASSERT)
 #if defined(RX_PLATFORM_WINDOWS)
-  DebugBreak();
+        DebugBreak();
 #elif defined(RX_PLATFORM_LINUX)
-  raise(SIGINT);
+        raise(SIGINT);
 #endif
 #else
-  abort(contents.data());
+        abort(contents.data());
 #endif
-}
+    }
 
 } // namespace rx
